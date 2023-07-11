f07587e99bd1c47006e7ee8458084d12881719af
AKSHAY648outlook <92755697+AKSHAY648outlook@users.noreply.github.com>
2023-07-11 12:28:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version = "~> 4.0"
     }
   }
 }
-
 provider "aws" {
-  region = "us-east-1"
-}
+   region     = "us-west-2"
+   access_key = "AKIA2RTRYBF6XQ6NDLUV"
+   secret_key = "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"
+} 
